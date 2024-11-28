6579e8cf8d2dbc64d6809910f049a6b8bb420b22
Behshad Ghorbani <behshad.ghorbani@enatel.net>
2024-11-28 14:32:06 +1300

    frontend charts added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component, Inject, NgZone, PLATFORM_ID } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import am5index from "@amcharts/amcharts5/index";
 import * as am5 from "@amcharts/amcharts5";
 import * as am5xy from "@amcharts/amcharts5/xy";
+import * as am5radar from "@amcharts/amcharts5/radar";
 import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 import { isPlatformBrowser } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
@@ -16,12 +18,23 @@ import { from, map, switchMap, tap, toArray } from "rxjs";
 })
 export class AppComponent {
   private rootChart1: am5.Root;
+  chart1: am5xy.XYChart;
+  series1: am5xy.LineSeries;
+  xAxis1: any;
+
+  private rootChart2: am5.Root;
+  chart2: am5xy.XYChart;
+  series2: am5xy.LineSeries;
+  xAxis2: any;
+
+  private rootChart3: am5.Root;
+  chart3: am5xy.XYChart;
+  series3: am5xy.LineSeries;
+  xAxis3: any;
+
   data;
   options = [];
   selectedOption = new FormControl({});
-  chart: am5xy.XYChart;
-  series: am5xy.LineSeries;
-  xAxis: any;
 
   constructor(
     @Inject(PLATFORM_ID) private platformId: Object,
@@ -62,9 +75,13 @@ export class AppComponent {
 
   ngAfterViewInit() {
     this.rootChart1 = am5.Root.new("chart-1");
+    this.rootChart2 = am5.Root.new("chart-2");
+    this.rootChart3 = am5.Root.new("chart-3");
+
     this.getLogs()
       .pipe(
         tap((data) => {
+          this.calculateAverageOfSomething(data);
           const logs = [];
           data = data[0].message.data;
           data.forEach((d) => {
@@ -73,7 +90,10 @@ export class AppComponent {
               value: parseInt(d["End Current (A)"]),
             });
           });
-          this.chart1(logs);
+          this.browserOnly(() => {
+            this.initChart1(logs);
+            this.initChart2(logs);
+          });
         })
       )
       .subscribe();
@@ -83,96 +103,329 @@ export class AppComponent {
     const value = (event.target as HTMLSelectElement).value;
     const data = this.data.find(
       (charger) => charger.chargerSerialNumber === value
-    ).message.data;
+    );
+
+    const data1 = data.message.data;
     const logs = [];
-    data.forEach((d) => {
+
+    data1.forEach((d) => {
       logs.push({
         category: d["Finish Time"],
         value: parseInt(d["End Current (A)"]),
       });
     });
-    this.series.data.setAll(logs);
-    this.xAxis.data.setAll(logs);
+    this.series1.data.setAll(logs);
+    this.xAxis1.data.setAll(logs);
   }
 
-  chart1(data) {
-    this.browserOnly(() => {
-      this.rootChart1.setThemes([am5themes_Animated.new(this.rootChart1)]);
+  initChart1(data) {
+    this.rootChart1.setThemes([am5themes_Animated.new(this.rootChart1)]);
 
-      let chart = this.rootChart1.container.children.push(
-        am5xy.XYChart.new(this.rootChart1, {
-          panY: false,
-          layout: this.rootChart1.verticalLayout,
-        })
-      );
+    let chart = this.rootChart1.container.children.push(
+      am5xy.XYChart.new(this.rootChart1, {
+        panY: false,
+        layout: this.rootChart1.verticalLayout,
+      })
+    );
 
-      let yAxis = chart.yAxes.push(
-        am5xy.ValueAxis.new(this.rootChart1, {
-          renderer: am5xy.AxisRendererY.new(this.rootChart1, {}),
-        })
-      );
+    let yAxis = chart.yAxes.push(
+      am5xy.ValueAxis.new(this.rootChart1, {
+        renderer: am5xy.AxisRendererY.new(this.rootChart1, {}),
+      })
+    );
 
-      yAxis.children.push(
-        am5.Label.new(this.rootChart1, {
-          rotation: -90,
-          text: "End Current (A)",
-          y: am5.p50,
-          centerX: am5.p50,
-          centerY: am5.p50,
-          fontSize: "14px",
-          fill: am5.color(0xffffff),
-        })
-      );
-
-      yAxis.get("renderer").labels.template.set("fill", am5.color(0xffffff));
-
-      this.xAxis = chart.xAxes.push(
-        am5xy.CategoryAxis.new(this.rootChart1, {
-          renderer: am5xy.AxisRendererX.new(this.rootChart1, {}),
-          categoryField: "category",
-        })
-      );
-
-      this.xAxis.get("renderer").labels.template.setAll({
+    yAxis.children.push(
+      am5.Label.new(this.rootChart1, {
+        rotation: -90,
+        text: "End Current (A)",
+        y: am5.p50,
+        centerX: am5.p50,
+        centerY: am5.p50,
+        fontSize: "14px",
         fill: am5.color(0xffffff),
-        fontSize: "10px",
-      });
-      this.xAxis.children.push(
-        am5.Label.new(this.rootChart1, {
-          text: "Finish Time",
-          x: am5.p50,
-          centerX: am5.p50,
-          centerY: am5.p50,
-          fontSize: "14px",
-          fill: am5.color(0xffffff),
-        })
-      );
-      this.xAxis.data.setAll(data);
+      })
+    );
 
-      this.series = chart.series.push(
-        am5xy.SmoothedXLineSeries.new(this.rootChart1, {
-          name: "Series",
-          xAxis: this.xAxis,
-          yAxis: yAxis,
-          valueYField: "value",
-          categoryXField: "category",
-        })
-      );
-      this.series.data.setAll(data);
+    yAxis.get("renderer").labels.template.set("fill", am5.color(0xffffff));
 
-      let legend = this.chart.children.push(
-        am5.Legend.new(this.rootChart1, {})
-      );
-      legend.data.setAll(chart.series.values);
+    this.xAxis1 = chart.xAxes.push(
+      am5xy.CategoryAxis.new(this.rootChart1, {
+        renderer: am5xy.AxisRendererX.new(this.rootChart1, {}),
+        categoryField: "category",
+      })
+    );
 
-      this.chart.set("cursor", am5xy.XYCursor.new(this.rootChart1, {}));
+    this.xAxis1.get("renderer").labels.template.setAll({
+      fill: am5.color(0xffffff),
+      fontSize: "10px",
     });
+    this.xAxis1.children.push(
+      am5.Label.new(this.rootChart1, {
+        text: "Finish Time",
+        x: am5.p50,
+        centerX: am5.p50,
+        centerY: am5.p50,
+        fontSize: "14px",
+        fill: am5.color(0xffffff),
+      })
+    );
+    this.xAxis1.data.setAll(data);
+
+    this.series1 = chart.series.push(
+      am5xy.SmoothedXLineSeries.new(this.rootChart1, {
+        name: "Series",
+        xAxis: this.xAxis1,
+        yAxis: yAxis,
+        valueYField: "value",
+        categoryXField: "category",
+      })
+    );
+    this.series1.data.setAll(data);
+
+    let legend = this.chart1?.children.push(
+      am5.Legend.new(this.rootChart1, {})
+    );
+    legend?.data.setAll(chart.series.values);
+
+    this.chart1?.set("cursor", am5xy.XYCursor.new(this.rootChart1, {}));
+    this.chart1?.appear(1000, 100);
+  }
+
+  calculateAverageOfSomething(logs) {
+    const soh = [];
+    const terminationCellVoltage = [];
+
+    logs.forEach((d) => {
+      const avg = this.getAvg(d.message.data, "State Of Health (%)");
+      soh.push(avg);
+      terminationCellVoltage.push(
+        this.getAvg(d.message.data, "Termination Cell Voltage (V)")
+      );
+    });
+
+    const sum = soh.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+
+    const sum2 = terminationCellVoltage.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    const avg = sum / soh.length;
+    const avg2 = sum2 / terminationCellVoltage.length;
+    this.initChart2(avg);
+    this.initChart3(avg2);
+  }
+
+  getAvg(dataArray, property) {
+    const total = dataArray.reduce((sum, item) => {
+      const value = parseFloat(item[property]);
+      return sum + (isNaN(value) ? 0 : value);
+    }, 0);
+
+    const average = total / dataArray.length;
+    return average;
+  }
+
+  initChart2(soh) {
+    this.rootChart2.setThemes([am5themes_Animated.new(this.rootChart2)]);
+
+    var chart = this.rootChart2.container.children.push(
+      am5radar.RadarChart.new(this.rootChart2, {
+        panX: false,
+        panY: false,
+        startAngle: -180,
+        endAngle: 0,
+      })
+    );
+
+    var axisRenderer = am5radar.AxisRendererCircular.new(this.rootChart2, {
+      innerRadius: -10,
+      strokeOpacity: 1,
+      strokeWidth: 2,
+      strokeGradient: am5.LinearGradient.new(this.rootChart2, {
+        rotation: 0,
+        stops: [
+          { color: am5.color(0x19d228) },
+          { color: am5.color(0xf4fb16) },
+          { color: am5.color(0xf6d32b) },
+          { color: am5.color(0xfb7116) },
+        ],
+      }),
+    });
+
+    var xAxis = chart.xAxes.push(
+      am5xy.ValueAxis.new(this.rootChart2, {
+        maxDeviation: 0,
+        min: 0,
+        max: 100,
+        strictMinMax: true,
+        renderer: axisRenderer,
+      })
+    );
+
+    var axisDataItem = xAxis.makeDataItem({});
+    axisDataItem.set("value", 0);
+
+    const hand = am5radar.ClockHand.new(this.rootChart2, {
+      pinRadius: 40,
+      radius: am5.percent(100),
+      innerRadius: 40,
+      bottomWidth: 7,
+      topWidth: 0,
+    });
+
+    hand.pin.setAll({
+      fillOpacity: 0,
+      strokeOpacity: 1,
+      stroke: am5.color(0xffffff),
+      strokeWidth: 3,
+    });
+    hand.hand.setAll({
+      fillOpacity: 0,
+      strokeOpacity: 1,
+      stroke: am5.color(0xffffff),
+      strokeWidth: 5,
+    });
+
+    const bullet = axisDataItem.set(
+      "bullet",
+      am5xy.AxisBullet.new(this.rootChart2, {
+        sprite: hand,
+      })
+    );
+
+    const label = chart.radarContainer.children.push(
+      am5.Label.new(this.rootChart2, {
+        centerX: am5.percent(50),
+        textAlign: "center",
+        centerY: am5.percent(50),
+        fontSize: "2em",
+      })
+    );
+
+    label.set("fill", am5.color(0xffffff));
+    const rounded = parseFloat(soh.toFixed(1));
+    label.set("text", Math.round(rounded).toString() + "%");
+
+    xAxis.createAxisRange(axisDataItem);
+
+    axisDataItem.get("grid").set("visible", true);
+    axisDataItem.get("tick").set("visible", true);
+
+    axisDataItem.animate({
+      key: "value",
+      to: rounded,
+      duration: 800,
+      easing: am5.ease.out(am5.ease.cubic),
+    });
+
+    chart.appear(1000, 100);
+  }
+
+  initChart3(terminationCellVoltage) {
+    this.rootChart3.setThemes([am5themes_Animated.new(this.rootChart3)]);
+
+    var chart = this.rootChart3.container.children.push(
+      am5radar.RadarChart.new(this.rootChart3, {
+        panX: false,
+        panY: false,
+        startAngle: -180,
+        endAngle: 0,
+      })
+    );
+
+    var axisRenderer = am5radar.AxisRendererCircular.new(this.rootChart3, {
+      innerRadius: -10,
+      strokeOpacity: 1,
+      strokeWidth: 2,
+      strokeGradient: am5.LinearGradient.new(this.rootChart3, {
+        rotation: 0,
+        stops: [
+          { color: am5.color(0x19d228) },
+          { color: am5.color(0xf4fb16) },
+          { color: am5.color(0xf6d32b) },
+          { color: am5.color(0xfb7116) },
+        ],
+      }),
+    });
+
+    var xAxis = chart.xAxes.push(
+      am5xy.ValueAxis.new(this.rootChart3, {
+        maxDeviation: 0,
+        min: 0,
+        max: 100,
+        strictMinMax: true,
+        renderer: axisRenderer,
+      })
+    );
+
+    var axisDataItem = xAxis.makeDataItem({});
+    axisDataItem.set("value", 0);
+
+    const hand = am5radar.ClockHand.new(this.rootChart3, {
+      pinRadius: 40,
+      radius: am5.percent(100),
+      innerRadius: 40,
+      bottomWidth: 7,
+      topWidth: 0,
+    });
+
+    hand.pin.setAll({
+      fillOpacity: 0,
+      strokeOpacity: 1,
+      stroke: am5.color(0xffffff),
+      strokeWidth: 3,
+    });
+    hand.hand.setAll({
+      fillOpacity: 0,
+      strokeOpacity: 1,
+      stroke: am5.color(0xffffff),
+      strokeWidth: 5,
+    });
+
+    const bullet = axisDataItem.set(
+      "bullet",
+      am5xy.AxisBullet.new(this.rootChart3, {
+        sprite: hand,
+      })
+    );
+
+    const label = chart.radarContainer.children.push(
+      am5.Label.new(this.rootChart3, {
+        centerX: am5.percent(50),
+        textAlign: "center",
+        centerY: am5.percent(50),
+        fontSize: "2em",
+      })
+    );
+
+    label.set("fill", am5.color(0xffffff));
+    const rounded = parseFloat(terminationCellVoltage.toFixed(1));
+    label.set("text", Math.round(rounded).toString() + "%");
+
+    xAxis.createAxisRange(axisDataItem);
+
+    axisDataItem.get("grid").set("visible", true);
+    axisDataItem.get("tick").set("visible", true);
+
+    axisDataItem.animate({
+      key: "value",
+      to: rounded,
+      duration: 800,
+      easing: am5.ease.out(am5.ease.cubic),
+    });
+
+    chart.appear(1000, 100);
   }
 
   ngOnDestroy() {
     this.browserOnly(() => {
       if (this.rootChart1) {
         this.rootChart1.dispose();
+      }
+      if (this.rootChart2) {
+        this.rootChart2.dispose();
       }
     });
   }
